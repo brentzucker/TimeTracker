626ca6c297ae9e522ba34391954b00efb784ee04
brentzucker <brentzucker@gmail.com>
2015-03-02 10:48:38 -0500

    Updated Control class and test/demo file
    Added functions to create a client, add a client purchase and remove a
    client and remove client purchases
    
    Also created functions that insert into each specific table.
    
    test_demo.php has 2 functions to test our database.
    testclient() adds and removes clients and purchases
    testemployee() adds and removes developers and info

--- Database.php
+++ Database.php
@@ -14,57 +14,132 @@ function db_query($query)
  *
  */
 
-function createEmployee($team_, $username_, $position_, $password_, $firstname_, $lastname_, $phone_, $email_, $address_, $city_, $state_)
+function createEmployee($team, $username, $position, $password, $firstname, $lastname, $phone, $email, $address, $city, $state)
 {
-	newDeveloper($team_, $username_, $position_);
-	newCredentials($username_, $password_);
-	newContact($username_, $firstname_, $lastname_, $phone_, $email_, $address_, $city_, $state_);
+	newDeveloper($team, $username, $position);
+	newCredentials($username, $password);
+	newContact($username, $firstname, $lastname, $phone, $email, $address, $city, $state);
 }
 
-function removeEmployee($username_)
+function deleteEmployee($username)
 {
-	removeCredentials($username_);
-	removeContact($username_);
-	removeDeveloper($username_);
+	removeCredentials($username);
+	removeContact($username);
+	removeDeveloper($username);
 }
 
+function createClient($ClientName, $StartDate, $Firstname, $Lastname, $Phone, $Email, $Address, $City, $State)
+{
+	newClient($ClientName, $StartDate);
+	newClientContact($ClientName, $Firstname, $Lastname, $Phone, $Email, $Address, $City, $State);
+}
+
+function deleteClient($ClientName)
+{
+	removeClientContact($ClientName);
+	removeClient($ClientName);
+}
+
+function createClientPurchase($ClientName, $HoursPurchased, $PurchaseDate)
+{
+	newClientPurchases($ClientName, $HoursPurchased, $PurchaseDate);
+}
+
+function deleteClientPurchase($ClientName)
+{
+	removeClientPurchases($ClientName);
+}
 /* The following functions directly query the database. 
  *
  */
 
-function newDeveloper($team_, $username_, $position_)
+function newDeveloper($team, $username, $position)
 {
-	$sql = "INSERT INTO Developer(Team, Username, Position) VALUES ('$team_','$username_', '$position_')";
+	$sql = "INSERT INTO Developer(Team, Username, Position) VALUES ('$team','$username', '$position')";
 	db_query($sql);
 }
 
-function removeDeveloper($username_)
+function removeDeveloper($username)
 {
-	$sql = "DELETE FROM Developer WHERE Username='$username_'";
+	$sql = "DELETE FROM Developer WHERE Username='$username'";
 	db_query($sql);
 }
 
-function newCredentials($username_, $password_)
+function newCredentials($username, $password)
 {
-	$sql = "INSERT INTO Credentials(Username, Password) VALUES ('$username_', '$password_')";
+	$sql = "INSERT INTO Credentials(Username, Password) VALUES ('$username', '$password')";
 	db_query($sql);
 }
 
-function removeCredentials($username_)
+function removeCredentials($username)
 {
-	$sql = "DELETE FROM Credentials WHERE Username='$username_'";
+	$sql = "DELETE FROM Credentials WHERE Username='$username'";
 	db_query($sql);
 }
 
-function newContact($username_, $firstname_, $lastname_, $phone_, $email_, $address_, $city_, $state_)
+function newContact($username, $firstname, $lastname, $phone, $email, $address, $city, $state)
 {
-	$sql = "INSERT INTO Contact(Username, Firstname, Lastname, Phone, Email, Address, City, State) VALUES ('$username_', '$firstname_', '$lastname_', '$phone_', '$email_', '$address_', '$city_', '$state_')";
+	$sql = "INSERT INTO Contact(Username, Firstname, Lastname, Phone, Email, Address, City, State) VALUES ('$username', '$firstname', '$lastname', '$phone', '$email', '$address', '$city', '$state')";
 	db_query($sql);
 }
 
-function removeContact($username_)
+function removeContact($username)
 {
-	$sql = "DELETE FROM Contact WHERE Username='$username_'";
+	$sql = "DELETE FROM Contact WHERE Username='$username'";
+	db_query($sql);
+}
+
+function newClient($ClientName, $StartDate)
+{
+	$sql = "INSERT INTO Client(ClientName, StartDate) VALUES ('$ClientName', '$StartDate')";
+	db_query($sql);
+}
+
+function removeClient($ClientName)
+{
+	$sql = "DELETE FROM Client WHERE ClientName='$ClientName'";
+	db_query($sql);
+}
+
+function newClientContact($ClientName, $Firstname, $Lastname, $Phone, $Email, $Address, $City, $State)
+{
+	$sql = "INSERT INTO ClientContact(ClientName, Firstname, Lastname, Phone, Email, Address, City, State) VALUES ('$ClientName', '$Firstname', '$Lastname', '$Phone', '$Email', '$Address', '$City', '$State')";
+	db_query($sql);
+}
+
+function removeClientContact($ClientName)
+{
+	$sql = "DELETE FROM ClientContact WHERE ClientName='$ClientName'";
+	db_query($sql);
+}
+
+function newClientPurchases($ClientName, $HoursPurchased, $PurchaseDate)
+{
+	$sql = "INSERT INTO ClientPurchases(ClientName, HoursPurchased, PurchaseDate) VALUES ('$ClientName', '$HoursPurchased', '$PurchaseDate')";
+	db_query($sql);
+}
+
+function removeClientPurchases($ClientName)
+{
+	$sql = "DELETE FROM ClientPurchases WHERE ClientName='$ClientName'";
+	db_query($sql);
+}
+
+function newProjects($ProjectID, $ClientName, $ProjectName, $Description)
+{
+	$sql = "INSERT INTO Projects(ProjectID, ClientName, ProjectName, Description) VALUES ('$ProjectID', '$ClientName', '$ProjectName', '$Description')";
+	db_query($sql);
+}
+
+function newTasks($TaskID, $ClientName, $ProjectID, $TaskName, $Description)
+{
+	$sql = "INSERT INTO Tasks(TaskID, ClientName, ProjectID, TaskName, Description) VALUES ('$TaskID', '$ClientName', '$ProjectID', '$TaskName', '$Description')";
+	db_query($sql);
+}
+
+function newTimeSheet($TimeLogID, $Username, $ClientName, $ProjectID, $TaskID, $TimeIn, $TimeOut)
+{
+	$sql = "INSERT INTO TimeSheet(TimeLogID, Username, ClientName, ProjectID, TaskID, TimeIn, TimeOut) VALUES ('$TimeLogID', '$Username', '$ClientName', $'ProjectID', '$TaskID', '$TimeIn', '$TimeOut')";
 	db_query($sql);
 }
 ?>
--- test_demo.php
+++ test_demo.php
@@ -21,34 +21,64 @@ function test($query)
 	echo '</table>';
 }
 
+function testEmployee()
+{
+	createEmployee('SE', 'b.zucker', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
+	createEmployee('SE', 'd.rhodes', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
+	createEmployee('SE', 't.land', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
+	createEmployee('SE', 'm.graessle', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
+
+	echo "<h3>Developers</h3>";
+	test("SELECT * 
+	FROM Developer 
+	LEFT JOIN Contact 
+	ON Developer.Username = Contact.Username
+	LEFT JOIN Credentials 
+	ON Developer.Username = Credentials.Username
+	");
+
+	deleteEmployee("b.zucker");
+	deleteEmployee("d.rhodes");
+	deleteEmployee("t.land");
+	deleteEmployee("m.graessle");
+
+	test("SELECT * 
+	FROM Developer 
+	LEFT JOIN Contact 
+	ON Developer.Username = Contact.Username
+	LEFT JOIN Credentials 
+	ON Developer.Username = Credentials.Username
+	");
+}
+
+function testClient()
+{
+	createClient('The Business', '1993-06-20', 'LeRoy', 'Jenkins', '1234567890', 'leeroy@gmail.com', 'The streets', 'Las Vegas', 'NV');
+	
+	echo "<h3>Client Info</h3>";
+	test("SELECT ClientContact.*, StartDate FROM Client, ClientContact WHERE (Client.clientname=ClientContact.clientname)");
+	
+	createClientPurchase('The Business','0000-00-00 10-30-00', '2015-02-14');
+
+	echo "<h3>Client Purchases</h3>";
+	test("SELECT * FROM ClientPurchases");
+
+	echo "<h3>Client Info With Purchases</h3>";
+	test("SELECT ClientContact.*, StartDate, PurchaseID, HoursPurchased, PurchaseDate FROM Client, ClientContact, ClientPurchases WHERE (Client.clientname=ClientContact.clientname) AND (Client.clientname=ClientPurchases.clientname)");
+	
+	deleteClientPurchase('The Business');
+	test("SELECT * FROM ClientPurchases");
+
+	deleteClient('The Business');
+	test("SELECT * FROM Client c, ClientPurchases cp, ClientContact cc WHERE (c.ClientName=cp.ClientName)");	
+}
+
 /* Call Operations to test Database
  *
  */
 
-createEmployee('SE', 'b.zucker', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
-createEmployee('SE', 'd.rhodes', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
-createEmployee('SE', 't.land', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
-createEmployee('SE', 'm.graessle', 'Developer', 'bz', 'Brent', 'Zucker', '4045801384', 'brentzucker@gmail.com', 'Columbia St', 'Milledgeville', 'GA');
+testEmployee();
+testClient();
+echo 'done';
 
-
-test("SELECT * 
-FROM Developer 
-LEFT JOIN Contact 
-ON Developer.Username = Contact.Username
-LEFT JOIN Credentials 
-ON Developer.Username = Credentials.Username
-");
-
-removeEmployee("b.zucker");
-removeEmployee("d.rhodes");
-removeEmployee("t.land");
-removeEmployee("m.graessle");
-
-test("SELECT * 
-FROM Developer 
-LEFT JOIN Contact 
-ON Developer.Username = Contact.Username
-LEFT JOIN Credentials 
-ON Developer.Username = Credentials.Username
-");
 ?>
