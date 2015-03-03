b04ff4de3216cbc4ca57b7e024e5e063d7272904
Ryan Graessle <max.graessle@gmail.com>
2015-03-02 23:43:06 -0500

    Updated Client Form
    Created the hidden date field
    Updated the styles on the form

--- clientForm.php
+++ clientForm.php
@@ -1,8 +1,21 @@
 <html>
 <head>
+<title>Client Form</title>
 <style>
 	.client_form {
-		text-align: center;
+		margin: auto;
+		position: relative;
+		width:550px;
+		height:450px;
+		padding:10px;
+		border: 1px solid #999;
+		line-height: 24px;
+		border-radius: 5px;
+	}
+	
+	input {
+		display: block;
+		width: 375px;
 	}
 
 </style>
@@ -14,17 +27,19 @@
 <?php
 require_once 'Database.php';
 
+$date = date("Y-m-d");
+
 echo<<< _END
-	<form class="client_form" method="post" action="client_form.php"><br /><br />
-		Client Name:<input type="text" name="client_name" /><br /><br />
-		First Name:<input type="text" name="first_name" /><br /><br />
-		Last Name:<input type="text" name="last_name" /><br /><br />
-		Phone:<input type="text" name="phone" /><br /><br />
-		Email:<input type="email" name="email" /><br /><br />
-		Address:<input type="text" name="address" /><br /><br />
-		City:<input type="text" name="city" /><br /><br />
-		State:<input type="text" name="state" /><br /><br />
-		<input type="date" name="start_date" value="" /><br /><br />
+	<form class="client_form" method="post" action="clientTest.php">
+		Client Name<input type="text" name="client_name" />
+		First Name<input type="text" name="first_name" />
+		Last Name<input type="text" name="last_name" />
+		Phone<input type="text" name="phone" />
+		Email<input type="email" name="email" />
+		Address<input type="text" name="address" />
+		City<input type="text" name="city" />
+		State<input type="text" name="state" />
+		<input type="hidden" name="start_date" value="$date" />
 		<input type="submit" />
 	</form>
 _END;
