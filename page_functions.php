d018f28ba10942d088764493c5da9f67a2eb221e
Ryan Graessle <max.graessle@gmail.com>
2015-03-23 20:05:48 -0400

    Created Page Functions file
    File to include header, footer and other functions used on pages

---
+++ page_functions.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'include.php';
+
+function header($title); //Function to load the header of the webpage. Takes in the title of the page.
+{
+echo<<<_END
+
+<html>
+	<head>
+		<title>$title</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+		<div class="header-wrap">
+			<div class="logo">
+				<a><img src="Logo Here" /></a>
+			<div>
+			<div class="menu">
+				<p>//**If we want a menu it will go here**//</p>
+			</div>
+		</div>
+_END;
+}
+
+function footer(); //Function to load the footer of the webpage and close out the HTML.
+{
+echo<<<_END
+
+	</body>
+</html>
+
+_END;
+}
+
+?>
