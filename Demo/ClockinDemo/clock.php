c7692c3b6352c25e8431e690f4795740a22f924e
brentzucker <brentzucker@gmail.com>
2015-04-01 21:07:39 -0400

    Updated Clockin

---
+++ Demo/ClockinDemo/clock.php
@@ -0,0 +1,18 @@
+<?php
+require_once(__DIR__.'/../../include.php');
+
+session_start();
+
+//Stores the task selected in the 'currentLog' session variable
+if(isset($_POST['Task_Selected']))
+	$_SESSION['currentLog']['task'] = $_POST['Task_Selected'];
+	
+echo '<h1>' . $_SESSION['Developer']->getUsername() . ' is logged in</h1>';
+
+echo '<h2>' . $_SESSION['currentLog']['task'] . ' was selected</h2>';
+echo '<h3>Clock In</h3>';
+
+clockForm($_SESSION['Developer'], $_SESSION['currentLog']['task']);
+
+printTimeLogTable($_SESSION['currentLog']['task']);
+?>
