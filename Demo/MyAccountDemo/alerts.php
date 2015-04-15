121a026811355acf5eec509514029f7ec30ce337
brentzucker <brentzucker@gmail.com>
2015-04-15 08:19:59 -0400

    update alerts
    This form will update the alert settings so a developer will get custom
    warnings on when contracts and hours expire

--- Demo/MyAccountDemo/alerts.php
+++ Demo/MyAccountDemo/alerts.php
@@ -1,5 +1,29 @@
 <?php
 require_once(__DIR__.'/../../include.php');
-//This is where you update your alert settings
-echo 'This is where you update your alert settings';
+
+session_start();
+
+echo '<h1>Update Alerts</h1>';
+
+updateAlertsForm();
+
+function updateAlertsForm()
+{
+	echo '<form action="" method="POST">';
+	echo '<label>Days Before a Contract Expires:</labels>';
+	echo '<input type="number" name="days" value="100">';
+	echo '<br>';
+	echo '<label>Hours Left on Contract:</label>';
+	echo '<input type="number" name="hours" value="10">';
+	echo '<br>';
+	echo '<input type="submit" value="Update Alerts">';
+	echo '</form>';
+
+	if(isset($_POST['days']) && isset($_POST['hours']))
+	{
+		echo '<h3>Alerts has been updated.</h3>';
+		echo '<h4>Days: ' . $_POST['days'] .'</h4>';
+		echo '<h4>Hours: ' . $_POST['hours'] . '</h4>';
+	}
+}
 ?>
