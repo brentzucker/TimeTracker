324f2e2ca7f74cb8e531e3c744c8aff808dc6053
Maxg93 <max.graessle@gmail.com>
2015-04-18 17:46:52 -0400

    Fixed missing php opening tag on all pages

--- Bootstrap/clock.php
+++ Bootstrap/clock.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require_once(__DIR__.'/../include.php');
 
 session_start();
--- Bootstrap/delete_client.php
+++ Bootstrap/delete_client.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 require_once(__DIR__.'/../include.php');
 require_once(__DIR__.'/page_functions.php');
