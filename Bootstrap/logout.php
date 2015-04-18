638aa3f40fbe0b8934467988f935a3bdd4be8d94
brentzucker <brentzucker@gmail.com>
2015-04-18 17:12:24 -0400

    Cleaned up file directory, updated logout
    moved test files to test directory
    
    moved sql files to sql directory
    
    deleted old files that we never used
    
    updated logout to destroy the session

---
+++ Bootstrap/logout.php
@@ -0,0 +1,7 @@
+<?php
+require_once (__DIR__ . '/../include.php');
+
+session_start();
+
+session_destroy();
+?>
--- createUser.sql
+++
@@ -1,3 +0,0 @@
-GRANT USAGE ON *.* TO 'System'@'localhost' IDENTIFIED BY PASSWORD '*576EE5B74C20E68F2A5A240F3E408E6DE43DD73F';
-
-GRANT ALL PRIVILEGES ON `clientdevelopmentmanagement_database`.`clientdevelopmentmanagement_database` TO 'System'@'localhost';
