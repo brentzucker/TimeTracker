1388379442c9123bbfca3d2983a60be97c3427db
Maxg93 <max.graessle@gmail.com>
2015-04-18 12:19:14 -0400

    Cleaned up front end and page functions
    - New front end visuals
    - Cleaned up login page and styles
    - Update page functions

--- Bootstrap/login_main.php
+++ Bootstrap/login_main.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once(__DIR__.'/../include.php');
 require_once(__DIR__.'/page_functions.php');
 
@@ -6,64 +7,8 @@ if(isset($_POST['submit']))
 {
 	checkLogin($_POST['username'], $_POST['password']);
 }
+
+open_login("Login");
+close_login();
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Login</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<link rel="icon" href="images/fav.jpg">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-	
-	<?php
-	
-echo<<<_END
-
-  	<img class="img-responsive center-block" src="http://i.imgur.com/dupUHP0.jpg" />
-    <div class="container">
-      <form class="form-signin" action="" method="POST">
-        <h2 class="form-signin-heading">Please Sign In</h2>
-        <label for="inputEmail" class="sr-only">Username</label>
-        <input type="text" name="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Sign In</button>
-      </form>
-
-_END;
-?>
-
-    </div> <!-- /container -->
-
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  </body>
-</html>
