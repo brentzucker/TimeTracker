d9b46d4f2bc4d55bf5ddcd79362ccb3c5770392a
brentzucker <brentzucker@gmail.com>
2015-04-18 11:41:47 -0400

    Developer new function deleteProject()
    delete project consumes a project object.
    
    Deletes every developer assignment from the same team and deletes the
    team assignment
    
    projects will still remain in database to keep timelogs the same

--- EntityClasses/Developer.php
+++ EntityClasses/Developer.php
@@ -470,5 +470,13 @@ class Developer
 
 		removeTeamAssignment( $this->getTeam() , $clientObject->getClientname() , 'Client');
 	}
+
+	function deleteProject($projectObject)
+	{
+		foreach( ( new Team( $this->getTeam() ) )->getDeveloperList() as $developer)
+			$developer->unassignProject($projectObject);
+
+		removeTeamAssignment( $this->getTeam() , $projectObject->getProjectID() , 'Project');
+	}
 }
 ?>
