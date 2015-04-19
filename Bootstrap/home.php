4644180ca34a784abab6cd75b3abecee52ac2762
brentzucker <brentzucker@gmail.com>
2015-04-19 10:26:51 -0400

    Home page shows last login, index start session
    home.php
    - if you’ve clocked in before it will show you who you last clocked in
    for and what time and day and the clients profile page
    
    page_functions.php
    - cleaned up a little
    - getWrongLoginError() - prints the error message for a incorrect login
    -  modified checkLogin
    — returns false if login was unsuccessful
    
    index.php (login page)
    - if the login returns false print getWrongLoginError()
    - else print page normally
    
    demo_functions.php
    - getClientProfile() consumes a clientName and prints out their profile
    tables
    - modified viewClientPorfiles() - if client has been selected call
    getClientProfile(client_selected)

--- Bootstrap/home.php
+++ Bootstrap/home.php
@@ -1,17 +1,15 @@
 <?php
 require_once(__DIR__.'/../include.php');
-require_once(__DIR__.'/page_functions.php');
 
 session_start();
 
 open_html("Home");
 
-//Keep all content in the div #page-content-wrapper
-
+/*
+ * Keep all content in the div #page-content-wrapper
+ */
 echo '<main id="page-content-wrapper">'; 
 echo '<div class="col-lg-12">';
-
-//Content goes here. 
 
 //Custom Greeting Message
 if(localtime(time(), true)['tm_hour'] < 11 && localtime(time(), true)['tm_hour'] > 3)
@@ -23,12 +21,21 @@ elseif(localtime(time(), true)['tm_hour'] > 16)
 else
 	echo '<h1>Welcome back ' . $_SESSION['Developer']->getContact()->getFirstname() . '!</h1>';
 
-if(localtime(time(), true)['tm_hour'] / 12 > 0)	
+if(localtime(time(), true)['tm_hour'] > 12)	
 	echo '<h5>The current time is ' . localtime(time(), true)['tm_hour'] % 12 . ":" . localtime(time(), true)['tm_min'] . ' pm</h5>';
 else 
 	echo '<h5>The current time is ' . localtime(time(), true)['tm_hour'] % 12 . ":" . localtime(time(), true)['tm_min'] . ' am</h5>';
 
+//If they have clocked in before
+if(count($_SESSION['Developer']->getTimeLog()) > 0)
+{
+	$last_timeObject = $_SESSION['Developer']->getTimeLog()[ count($_SESSION['Developer']->getTimeLog()) - 1 ];
+	echo '<h3>You last clocked out on ' . (new DateTime($last_timeObject->getTimeOut()))->format('l F jS Y') . (new DateTime($last_timeObject->getTimeOut()))->format(' \a\t g:ia') . '.</h3>';
+	echo '<h3>You were working on ' . $last_timeObject->getClientname() . ', ' . (new Projects ($last_timeObject->getProjectId()))->getProjectName() . ', ' . (new Tasks ($last_timeObject->getTaskId()))->getTaskName() . '.</h3>';
 
+	//Load Client Profile Page of last clock in
+	getClientProfile($last_timeObject->getClientname());
+}
 
 echo '</div>';
 echo '</div>';
@@ -37,5 +44,4 @@ echo '</div>';
 echo '</main>';
 
 close_html();
-
 ?>
