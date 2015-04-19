e69e31c87461a5ac54c49a74fe9788a648a6ecee
brentzucker <brentzucker@gmail.com>
2015-04-18 21:46:13 -0400

    Custom Greeting message on home page
    Tells the time of day and based off the time of day says good morning,
    good afternoon, or good evening and the users first name

--- Bootstrap/home.php
+++ Bootstrap/home.php
@@ -1,23 +1,40 @@
 <?php
-
 require_once(__DIR__.'/../include.php');
 require_once(__DIR__.'/page_functions.php');
+
+session_start();
 
 open_html("Home");
 
 //Keep all content in the div #page-content-wrapper
 
-echo<<<_END
-<div id="page-content-wrapper"> 
-                <div class="col-lg-12">
-                    <h1>Home</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-</div>
-_END;
+echo '<main id="page-content-wrapper">'; 
+echo '<div class="col-lg-12">';
+
+//Content goes here. 
+
+//Custom Greeting Message
+if(localtime(time(), true)['tm_hour'] < 11 && localtime(time(), true)['tm_hour'] > 3)
+	echo '<h1>Good Morning ' . $_SESSION['Developer']->getContact()->getFirstname() . '!</h1>';
+elseif(localtime(time(), true)['tm_hour'] > 11 && localtime(time(), true)['tm_hour'] < 16)	
+	echo '<h1>Good Afternoon ' . $_SESSION['Developer']->getContact()->getFirstname() . '</h1>';
+elseif(localtime(time(), true)['tm_hour'] > 16)	
+	echo '<h1>Good Evening ' . $_SESSION['Developer']->getContact()->getFirstname() . '</h1>';
+else
+	echo '<h1>Welcome back ' . $_SESSION['Developer']->getContact()->getFirstname() . '!</h1>';
+
+if(localtime(time(), true)['tm_hour'] / 12 > 0)	
+	echo '<h5>The current time is ' . localtime(time(), true)['tm_hour'] % 12 . ":" . localtime(time(), true)['tm_min'] . ' pm</h5>';
+else 
+	echo '<h5>The current time is ' . localtime(time(), true)['tm_hour'] % 12 . ":" . localtime(time(), true)['tm_min'] . ' am</h5>';
+
+
+
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';    
+echo '</main>';
 
 close_html();
 
