e251864d31cc1a52efba6cfa253c2a003950bc54
brentzucker <brentzucker@gmail.com>
2015-04-20 16:53:12 -0400

    Home page has alerts
    bootstrap_functions
    - Called warning functions in alertBox()

--- Functions/bootstrap_functions.php
+++ Functions/bootstrap_functions.php
@@ -265,13 +265,14 @@ _END;
 
 function alertBox()
 {
-echo<<<_END
-		<div class="col-lg-3 alert-box">
+	echo '<div class="col-lg-3 alert-box">';
+	echo '<h2>Alerts</h2>';
+
+	warningExpiringContracts( $_SESSION['Developer']->getDaysExpirationWarning() );
+	warningLowHours( $_SESSION['Developer']->getHoursLeftWarning() );
 		
-			<h2>Alerts</h2>
-		
-		</div>
-_END;
+	echo '<h6><a href="update_alerts.php">Update Alert Settings</a></h6>';
+	echo '</div>';
 }
 
 ?>
